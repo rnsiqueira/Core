3533586e9b425f512da9737dfa395cb7da7f52f3
Rafael N. Siqueira <rafaeln.siqueira@hotmail.com>
2017-08-18 00:55:22 -0300

    Add files via upload

--- Core/PJava/src/Test/TestCurso.java
+++ Core/PJava/src/Test/TestCurso.java
@@ -1,7 +1,12 @@
 package Test;
 
 
+
+
+
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import Itens.Aula;
 import Itens.Curso;
@@ -12,10 +17,16 @@ public class TestCurso {
 		
 		
 		Curso cur = new Curso("Primeiro Curso", "Rafael");
-		cur.adiciona(new Aula("Primeira Aula", 35));
-		cur.adiciona(new Aula("Segunda Aula", 25));
-		Collections.sort(cur.getAulas());
-		System.out.println(cur.getAulas());
+		List<Aula> aulasNaoMutaveis = cur.getAulas();
+		cur.adiciona(new Aula("Primeira Aula ", 35));
+		cur.adiciona(new Aula("Segunda Aula ", 25));
+		System.out.println(aulasNaoMutaveis);
+		
+		List<Aula> listarAula = new ArrayList<>(aulasNaoMutaveis);
+		Collections.sort(listarAula);
+		System.out.println(listarAula +" \nTempo Total: "+ cur.getTempoTotal());
+		
+		
 		
 		
 	
